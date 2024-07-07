eb783523ed3286092eb5c62a233b5e9533eab97b
Mikkel ALMONTE--RINGAUD <mikkel@milescode.dev>
2024-07-07 21:29:19 +0200

    chore: add wip for press state store

---
+++ src/stores/pressed.tsx
@@ -0,0 +1,7 @@
+import { createStore } from "solid-js/store";
+
+// Keys that are currently active.
+// We'll keep this for CTRL or those types of keys.
+export const [pressed, setPressed] = createStore({
+
+});
